a728f3febd2d667ca5406bf317e73cba4446f984
MarceloAndresMendoza <marcelo@doblefoco.cl>
2023-07-10 20:29:25 -0400

    build: fixed base url for github pages compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,17 @@
 import { defineConfig } from 'astro/config';
+const devMode = import.meta.env.DEV;
+let siteURL = 'https://marceloandresmendoza.github.io/weather-report';
+let siteBase = '';
+if (devMode) {
+    siteURL = 'http://localhost:3000';
+    siteBase = '';
+}
+
 
 // https://astro.build/config
 export default defineConfig({
+    site: siteURL,
+    base: siteBase,
     output: 'static',
     outDir: './docs',
     build: {
