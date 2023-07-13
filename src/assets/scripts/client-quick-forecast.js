15acf36499ba191635395fa0f0bd393b2e8cedd3
MarceloAndresMendoza <marcelo@doblefoco.cl>
2023-07-13 12:09:23 -0400

    fix: Refactored code, better workflow control feat: APP workflow: GPS > IP > Weather data build: field testing build

--- src/assets/scripts/client-quick-forecast.js
+++ src/assets/scripts/client-quick-forecast.js
@@ -5,72 +5,121 @@
 // App starts
 // └ Saved location from localStorage?
 //  ├ Yes: [Use this instead] => [Get Weather Report (Saved location)]
-//  └ No: First time app loaded (Suggest press the getLocation button<)
+//  └ No: First time app loaded (Suggest press the getLocation button<) - DONE
 //   └ [GetLocation Button] Try to get Geolocation
 //     ├ Got coordinates => [Get Weather Report (Exact location)]
 //     └ Did'nt got coordinates
-//      └ Try to get IP from IPAPI.com
+//      └ Try to get IP from IPAPI.com - DONE
 //       ├ Got coordinates => [Get Weather Report (Aproximate location)]
 //       └ Didn't got IP => [Manual location set]
-const savedLocation = localStorage.getItem(savedLocation);
-if (savedLocation) {
-    // There is a saved location
-    
-} else {
-    // First time app load
-    
-}
+
+// ========================================
+// ===== Check if got any coordinates =====
+// ========================================
+const gotCoordinates = document.getElementById('gotcoords');
+
+// Create a new MutationObserver (check changes on the div element)
+const observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+        // Check if the mutation is an innerHTML change
+        if (mutation.type === 'childList') {
+            // ===> GET WEATHER
+            const lat = document.getElementById('latitude').innerText;
+            const lon = document.getElementById('longitude').innerText;
+            getForecast(lat, lon);
+        }
+    });
+});
+// Start observing the element
+observer.observe(gotCoordinates, {
+    childList: true
+});
+// =======================================
 
 
-const getIP = () => {
+// ========================================
+// ======= Get IP location via API ========
+// ========================================
+const getIPlocation = (onlyIp = "false") => {
     const url = "https://ipapi.co/json/";
     fetch(url)
         .then((response) => {
             return response.json();
         })
         .then((data) => {
-            // document.getElementById('data').innerHTML = data.image;
             document.getElementById("yourIP").innerHTML = data.ip;
-            document.getElementById("yourLocation").innerHTML =
-                data.city + ", " + data.country_name;
-            getForecast(data.latitude, data.longitude);
+            if (!onlyIp) {
+                document.getElementById("yourLocation").innerHTML = data.city + ", " + data.country;
+                document.getElementById("yourLocationLabel").innerHTML = "Your aproximated location:";
+                document.getElementById("latitude").innerHTML = data.latitude.toFixed(2);
+                document.getElementById("longitude").innerHTML = data.longitude.toFixed(2);
+                document.getElementById("gotcoords").innerHTML = 'true';
+                document.getElementById("geolocationinfo").innerHTML = 'obtained now via IP';
+            }
             return data;
+        })
+        .catch((error) => {
+            document.getElementById("yourIP").innerHTML = error;
+            return error;
         });
 };
+// ========================================
+
+const getReverseGeocode = (lat, lon) => {
+    // Get your precise location address
+    const url = `https://geocode.maps.co/reverse?lat=${lat}&lon=${lon}`;
+    fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            // console.log(data);
+            // alert(data.address.city + " " + data.address.country);
+            document.getElementById("yourLocationLabel").innerHTML = "Your precise location:";
+            document.getElementById("yourLocation").innerHTML = data.address.city + ", " + data.address.country;
+            return data;
+        })
+}
+
+function btGetLocation() {
+    // User presses the 'update' button.
+    if ("geolocation" in navigator) {
+        // Geolocation is available
+        let geoLocationTimeout = setTimeout(() => {
+            // In the case could not obtain gelocation coordinates, shows an error.
+            document.getElementById('geolocationinfo').innerHTML = "Couldn't get geolocation. Check permissions on your device."
+            // ===> Try IP location
+            getIPlocation();
+        }, 10000);
+        navigator.geolocation.getCurrentPosition((position) => {
+            const lat = position.coords.latitude.toFixed(2);
+            const lon = position.coords.longitude.toFixed(2);
+            document.getElementById('latitude').innerHTML = lat;
+            document.getElementById('longitude').innerHTML = lon;
+            document.getElementById("gotcoords").innerHTML = 'true';
+            document.getElementById("geolocationinfo").innerHTML = 'obtained now via geolocation';
+            getIPlocation(true);
+            getReverseGeocode(position.coords.latitude, position.coords.longitude);
+            // Got geolocation coords, cancel the timeout
+            clearTimeout(geoLocationTimeout);
+            // ===> Get weather data (precise)
+        });
+    } else {
+        // Geolocation is not available
+        document.getElementById('geolocationinfo').innerHTML = "Geolocation not available or not allowed by user."
+        // ===> Try IP location}
+        getIPlocation();
+    }
+}
+
+document.getElementById('btGetLocation').addEventListener('click', btGetLocation);
 
 const getForecast = (lat, lon) => {
-
-    getGeolocation();
-
-    function getGeolocation() {
-        if ("geolocation" in navigator) {
-            let geoLocationTimeout = setTimeout(() => {
-                document.getElementById('geolocation').innerHTML = "Couldn't get geolocation. Check permissions on your device."
-            }, 10000);
-            // document.getElementById('geolocation').innerHTML = "Geolocation available."
-            navigator.geolocation.getCurrentPosition((position) => {
-                if (gotPosition(position.coords.latitude, position.coords.longitude)) {
-                    clearTimeout(geoLocationTimeout);
-                }
-            });
-            // console.log('Geolocation available')
-        } else {
-            // console.log('Geolocation NOT available')
-            document.getElementById('geolocation').innerHTML = "Geolocation not available or not allowed by user."
-        }
-    }
-
-    function gotPosition(glat, glon) {
-        console.log('Got position:' + glat.toFixed(2) + ", " + glon.toFixed(2));
-        // console.log(lat + " " + lon);
-        document.getElementById('latitude').innerHTML = glat.toFixed(2);
-        document.getElementById('longitude').innerHTML =glon.toFixed(2);
-        lat = glat;
-        lon = glon;
-        document.getElementById('yourLocationLabel').innerHTML = "Your precise location:";
-        return true;
-    }
-
+    // First, save current coordinates on localStorage
+    localStorage.setItem("savedLocation", "true");
+    localStorage.setItem("latitude", lat);
+    localStorage.setItem("longitude", lon);
+    // Check weather on open-meteo API
     const url = `https://api.open-meteo.com/v1/forecast?current_weather=true&latitude=${lat}&longitude=${lon}&hourly=precipitation_probability&hourly=temperature_2m`;
     fetch(url)
         .then((response) => {
@@ -151,5 +200,20 @@ function monthName(num) {
 }
 
 
-
-getIP();
+const savedLocation = localStorage.getItem("savedLocation");
+if (savedLocation == null) {
+    // First time app load
+    document.getElementById("geolocationinfo").innerHTML = 'Press "Update" to obtain your current location.';
+    btGetLocation();
+} else {
+    // There is a saved location
+    // ===> Get weather data (saved)
+    const lat = localStorage.getItem("latitude");
+    const lon = localStorage.getItem("longitude");
+    document.getElementById('latitude').innerHTML = lat;
+    document.getElementById('longitude').innerHTML = lon;
+    document.getElementById("gotcoords").innerHTML = 'true';
+    document.getElementById("geolocationinfo").innerHTML = 'previously saved data, you can press "update"';
+    getIPlocation(true);
+    getReverseGeocode(lat, lon);
+}
